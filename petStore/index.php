d7f30248555e9198b242bfa252af60b804e37993
samuel <damosisamuel@gmail.com>
2023-01-24 16:48:11 +0100

    to php

---
+++ petStore/index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
+    <!-- bootsrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <!-- cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- header starts -->
+    <header class="container-fluid">
+        <!-- header top -->
+        <div class=" row header-t">
+            <div class="col-4 btb ">
+                <div class="row ">
+                    <div class="col-2 offset-2 pt-2">
+                        <li><i class="fa-solid fa-location-dot ml-4"></i></li>
+                        
+                    </div>
+                    <div class="col-6 pt-3 ">
+                        <h5>OUR OFFICE</h5>
+                        <p>123 street, New York, USA</p>
+                    </div>
+                 
+                </div>
+
+            </div>
+            <div class="col-4 btb">
+                <div class="row">
+                    <div class="col-2 offset-2 pt-2">
+                        <li><i class="fa-regular fa-envelope ml-4"></i></li>
+                        
+                    </div>
+                    <div class="col-6 pt-3">
+                        <h5>EMAIL US</h5>
+                        <p>mail@domain.com</p>
+                    </div>
+                 
+                </div>
+
+            </div>
+            <div class="col-4 btb">
+                <div class="row">
+                    <div class="col-2 offset-2 pt-2">
+                        <li><i class="fa-solid fa-phone ml-4"></i></li>
+                        
+                    </div>
+                    <div class="col-6 pt-3">
+                        <h5>CALL US</h5>
+                        <p>+012 345 6798</p>
+                    </div>
+                 
+                </div>
+
+            </div>
+
+        </div>
+        <!-- heder top -->
+<!-- header bottom -->
+        <div class=" row header-b">
+          <div class="col-6 d-flex navi mt-3 ">
+              <li style="font-size: 28px;"><i class="fa-solid fa-house mr-3"></i></li>
+              <h5>PET STORE</h5>
+
+          </div>
+          <div class="col-6">
+              <div class="row ">
+                  <div class="col-10 ">
+                      <ul class="d-flex justify-content-around navi mt-4">
+                          <li>Home</li>
+                          <li>About</li>
+                          <li>Service</li>
+                          <li>Product</li>
+                          <li>Pages</li>
+                      </ul>
+
+                  </div>
+                  <div class="col-2 navi pt-4 gbt">
+
+                    <li class="gbt">Contact</li>
+
+                 </div>
+
+              </div>
+
+        </div>
+          
+           
+
+        </div>
+        <!-- header bottom -->
+    </header>
+    <!-- header ends -->
+    <!-- main starts -->
+    <main>
+        <div class="jumbotron">
+            <div class="row ml-2">
+                <h1>PET SHOP</h1>
+
+                <h4>MAKE YOUR PETS HAPPY</h4>
+
+                <p>Dolore tempor clita lorem rebum kasd eirmod dolore diam eos kasd. Kasd clita ea justo est sed kasd erat clita sea</p>
+
+                <div class="row col-6">
+                    <div class="col-3 mybn">
+                        <button type="button" class="btn-lg btn-outline-dark">READ MORE</button>
+
+                        
+
+                    </div>
+                    <div class="col-5">
+                        <ul class="d-flex  min">
+                            <li><i class="mt-4 fa-sharp fa-solid fa-circle-play plybtn"></i>
+                              </li>
+                            <li class="liv mr-3">Play Video</li>
+        
+                          </ul>
+                        
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <section class="sect1 container mt-5">
+            <div class="row">
+                <div class="col-6">
+                    <img height="469px;" src="pet.jpeg" alt="">
+
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-12 ">
+                            <div class="rect pl-5">
+                                <p>ABOUT US</p>
+                                <h4>WE KEEP YOUR PETS HAPPY ALL TIME</h4>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="row ">
+                        <div class="col-12 mt-4">
+                            <p class="th">Diam dolor diam ipsum tempor sit. Clita erat ipsum et lorem stet no labore lorem sit clita duo justo magna dolore</p>
+
+                        </div>
+
+                    </div>
+                    <div class="row" style="background-color: #F3F3F3!important;">
+                        <div class="col-4 offset-2 mission mt-4 pt-1 text-center" >
+                            <p class="">OUR MISSION</p>
+                        </div>
+                        <div class="col-3 mt-4 pt-1 ml-5 text-center">
+
+                            <p style="color: #7AB730;">OUR VISSION</p>
+                        </div>
+                        <div class="col-12">
+                            <p>Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+        <section class="sect2 container mt-5">
+            <div class="row ">
+                <div class="col-6  rector">
+                    <div class="rect pl-5 ">
+                        <p>SERVICES</p>
+                        <h4>OUR EXCELLENT PET CARE SERVICES</h4>
+
+                    </div>
+                    <div class="col-4">
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </section>
+        <section class="sect3 container">
+            <!-- FIRST ROLL  -->
+            <div class="row ">
+                <div class="offset-1 col-5 pt-4  ft d-flex">
+                    <div style="list-style: none; ">
+                        <li><i class="fa-solid fa-house ict mt-3 mr-3" ></i></li>
+    
+                     </div>
+                    <div>
+                        <h2 style="font-size: 18px; font-weight: 700;">PET BOARDING</h2>
+    
+                        <p style="color: #656565;">Kasd dolor no lorem sit tempor at justo rebum rebum stet justo elitr dolor amet sit</p>
+    
+                        <ul class="d-flex " style="list-style: none;">
+                            <li class="gbb">Read more</li>
+                            <li><i class="fa-solid fa-arrow-right ml-2 gbb"></i></li>
+                        </ul>
+    
+                    </div>
+    
+                </div>
+                <div class="col-5 ml-4 pt-4 ft d-flex">
+                    <div style="list-style: none; ">
+
+                        <li><i class="fa-solid fa-fish ict mt-3 mr-3" ></i></li>
+    
+                     </div>
+                    <div>
+                        <h2 style="font-size: 18px; font-weight: 700;">PET FEEDING</h2>
+    
+                        <p>Kasd dolor no lorem sit tempor at justo rebum rebum stet justo elitr dolor amet sit</p>
+    
+                        <ul class="d-flex " style="list-style: none;">
+                            <li class="gbb">Read more</li>
+                            <li><i class="fa-solid fa-arrow-right ml-2 gbb"></i></li>
+                        </ul>
+    
+                    </div>
+    
+                </div>
+
+
+            </div>
+                        <!-- FIRST ROLL  -->
+             <!-- SECOND ROLL -->
+            <div class="row mt-5 ">
+                <div class="offset-1 col-5 pt-4  ft d-flex">
+                    <div style="list-style: none; ">
+                        <li><i class="fa-solid fa-dumbbell ict mt-3 mr-3" ></i></li>
+    
+                     </div>
+                    <div>
+                        <h2 style="font-size: 18px; font-weight: 700;">PET EXERCISE</h2>
+    
+                        <p style="color: #656565;">Kasd dolor no lorem sit tempor at justo rebum rebum stet justo elitr dolor amet sit</p>
+    
+                        <ul class="d-flex " style="list-style: none;">
+                            <li class="gbb">Read more</li>
+                            <li><i class="fa-solid fa-arrow-right ml-2 gbb"></i></li>
+                        </ul>
+    
+                    </div>
+    
+                </div>
+                <div class="col-5 ml-4 pt-4 ft d-flex">
+                    <div style="list-style: none; ">
+                        <li><i class="fa-solid fa-shield-dog ict mt-3 mr-3" ></i></li>
+    
+                     </div>
+                    <div>
+                        <h2 style="font-size: 18px; font-weight: 700;">PET GROOMING</h2>
+    
+                        <p>Kasd dolor no lorem sit tempor at justo rebum rebum stet justo elitr dolor amet sit</p>
+    
+                        <ul class="d-flex " style="list-style: none;">
+                            <li class="gbb">Read more</li>
+                            <li><i class="fa-solid fa-arrow-right ml-2 gbb"></i></li>
+                        </ul>
+    
+                    </div>
+    
+                </div>
+
+
+            </div>
+            <!-- SECOND ROLL -->
+            <!-- THIRD ROLL -->
+            <div class="row mt-5 ">
+                <div class="offset-1 col-5 pt-4  ft d-flex">
+                    <div style="list-style: none; ">
+                        
+                        <li><i class="fa-solid fa-user ict mt-3 mr-3" ></i></li>
+    
+                     </div>
+                    <div>
+                        <h2 style="font-size: 18px; font-weight: 700;">PET TRAINING</h2>
+    
+                        <p style="color: #656565;">Kasd dolor no lorem sit tempor at justo rebum rebum stet justo elitr dolor amet sit</p>
+    
+                        <ul class="d-flex " style="list-style: none;">
+                            <li class="gbb">Read more</li>
+                            <li><i class="fa-solid fa-arrow-right ml-2 gbb"></i></li>
+                        </ul>
+    
+                    </div>
+    
+                </div>
+                <div class="col-5 ml-4 pt-4 ft d-flex">
+                    <div style="list-style: none; ">
+                        <li><i class="fa-solid fa-suitcase-medical ict mt-3 mr-3" ></i></li>
+    
+                     </div>
+                    <div>
+                        <h2 style="font-size: 18px; font-weight: 700;">PET TREATMENT</h2>
+    
+                        <p>Kasd dolor no lorem sit tempor at justo rebum rebum stet justo elitr dolor amet sit</p>
+    
+                        <ul class="d-flex " style="list-style: none;">
+                            <li class="gbb">Read more</li>
+                            <li><i class="fa-solid fa-arrow-right ml-2 gbb"></i></li>
+                        </ul>
+    
+                    </div>
+    
+                </div>
+
+
+            </div>
+            <!-- THIRD ROLL -->
+            <!-- sect2 reduplicated -->
+            <section class="sect2 container mt-5">
+                <div class="row ">
+                    <div class="col-6  rector">
+                        <div class="rect pl-5 ">
+                            <p>PRODUCTS</p>
+                            <h4>PRODUCTS FOR YOUR BEST FRIENDS</h4>
+    
+                        </div>
+                        <div class="col-4">
+    
+                        </div>
+    
+    
+                    </div>
+    
+                </div>
+    
+            </section>
+                        <!-- sect2 reduplicated -->
+
+          
+          
+
+        </section>
+        
+       
+
+    </main>
+    <!-- main ends here -->
+    <!-- footer starrts here -->
+    <footer>
+
+    </footer>
+    <!-- footer ends here -->
+
+
+    
+</body>
+</html>
